894dd659b8f37264a5416c666cef573cd7853dcb
KangYeonwoo <kywoo26@naver.com>
2020-12-04 01:19:09 +0900

    Add basic LISP method.

--- PL_LISP_Interpreter/myLisp.cpp
+++ PL_LISP_Interpreter/myLisp.cpp
@@ -95,6 +95,122 @@ PNode func_NTH(std::list<PNode>& nodeList)
 	return *iter;
 }
 
+PNode func_CONS(std::list<PNode>& nodeList)
+{
+	nodeList.back()->childNodeList.push_front(nodeList.front());
+	return nodeList.back();
+}
+
+PNode func_REVERSE(std::list<PNode>& nodeList)
+{
+	std::shared_ptr<Node> NewNode(new Node(NODE_TYPE::LIST));
+	std::list<PNode>::iterator iter;
+	for (iter = nodeList.front()->childNodeList.begin(); iter != nodeList.front()->childNodeList.end(); iter++)
+		NewNode->childNodeList.push_front(*iter);
+	return NewNode;
+}
+
+PNode func_APPEND(std::list<PNode>& nodeList)
+{
+	std::shared_ptr<Node> NewNode(new Node(NODE_TYPE::LIST));
+	std::list<PNode>::iterator iter = nodeList.begin();
+	for (iter; iter != nodeList.end(); iter++)
+		for (std::list<PNode>::iterator subiter = (*iter)->childNodeList.begin(); subiter != (*iter)->childNodeList.end(); subiter++)
+			NewNode->childNodeList.push_back(*subiter);
+	return NewNode;
+}
+
+PNode func_LEGNTH(std::list<PNode>& nodeList)
+{
+	std::shared_ptr<Node> NewNode(new Node(NODE_TYPE::NUM,
+		std::to_string(nodeList.front()->childNodeList.size())));
+	return NewNode;
+}
+
+PNode func_MEMBER(std::list<PNode>& nodeList)
+{
+	PNode NewNode(new Node(NODE_TYPE::STR, "NIL"));
+
+	std::string targetValue = nodeList.front()->value;
+	std::list<PNode>::iterator iter = nodeList.back()->childNodeList.begin();
+
+	for (iter; iter != nodeList.back()->childNodeList.end(); iter++)
+	{
+		if ((*iter)->value == targetValue)
+			break;
+	}
+	if (iter != nodeList.back()->childNodeList.end())
+	{
+		nodeList.back()->childNodeList.erase(nodeList.back()->childNodeList.begin(), iter);
+		NewNode = nodeList.back();
+	}
+	return NewNode;
+}
+
+PNode func_ASSOC(std::list<PNode>& nodeList)
+{
+	std::shared_ptr<Node> NewNode(new Node(NODE_TYPE::LIST));
+
+	std::string key = nodeList.front()->value;
+	nodeList.pop_front();
+
+	std::list<PNode> target = nodeList.front()->childNodeList;
+	std::list<PNode>::iterator iter;
+	for (iter = target.begin(); iter != target.end(); iter++)
+	{
+		// Find Sub-LIST which has 'key' as first element.
+		if ((*iter)->childNodeList.front()->value == key)
+		{
+			NewNode->childNodeList = (*iter)->childNodeList;
+			break;
+		}
+	}
+
+	return NewNode;
+}
+
+PNode func_REMOVE(std::list<PNode>& nodeList)
+{
+	std::shared_ptr<Node> NewNode(new Node(NODE_TYPE::LIST));
+
+	std::string at = nodeList.front()->value;
+	nodeList.pop_front();
+
+	std::list<PNode> target = nodeList.front()->childNodeList;
+	std::list<PNode>::iterator iter;
+	for (iter = target.begin(); iter != target.end(); iter++)
+	{
+		// Remove 'at' (Ignore 'at' in LIST)
+		if ((*iter)->value != at)
+			NewNode->childNodeList.push_back(*iter);
+	}
+
+	return NewNode;
+}
+
+PNode func_SUBST(std::list<PNode>& nodeList)
+{
+	std::shared_ptr<Node> NewNode(new Node(NODE_TYPE::LIST));
+
+	std::string to = nodeList.front()->value;
+	nodeList.pop_front();
+	std::string from = nodeList.front()->value;
+	nodeList.pop_front();
+
+	std::list<PNode> target = nodeList.front()->childNodeList;
+	std::list<PNode>::iterator iter;
+	for (iter = target.begin(); iter != target.end(); iter++)
+	{
+		// Replace "from" with "to" in LIST
+		if ((*iter)->value == from)
+			(*iter)->value = to;
+
+		NewNode->childNodeList.push_back(*iter);
+	}
+
+	return NewNode;
+}
+
 //bMark(') 가 있을때 true
 //bMark(') 가 있으면 밑에 연산을 수행하지 않고 그냥 리턴함
 PNode interpret(PNode node, bool bMark = false)
@@ -118,7 +234,7 @@ PNode interpret(PNode node, bool bMark = false)
 		return node;
 	}
 
-	std::list<PNode> elements;
+	std::list<PNode> nodeArgs;
 	std::list<PNode>::iterator iter = ++node->childNodeList.begin();
 	bool mark = false;
 
@@ -133,49 +249,80 @@ PNode interpret(PNode node, bool bMark = false)
 			mark = true;
 			continue;
 		}
-		elements.push_back(interpret(*iter, mark));
+		nodeArgs.push_back(interpret(*iter, mark));
 		if (mark) mark = !mark;
 	}
 
 	if (node->childNodeList.front()->value == "+")
 	{
-		node = func_add(elements);
+		node = func_add(nodeArgs);
 	}
 	else if (node->childNodeList.front()->value == "SETQ")
 	{
-		node = func_SETQ(elements);
+		node = func_SETQ(nodeArgs);
 	}
 	else if (node->childNodeList.front()->value == "-")
 	{
-		node = func_sub(elements);
+		node = func_sub(nodeArgs);
 	}
 	else if (node->childNodeList.front()->value == "*")
 	{
-		node = func_mul(elements);
+		node = func_mul(nodeArgs);
 	}
 	else if (node->childNodeList.front()->value == "/")
 	{
-		node = func_div(elements);
+		node = func_div(nodeArgs);
 	}
 
 	else if (node->childNodeList.front()->value == "LIST")
 	{
-		node = func_LIST(elements);
+		node = func_LIST(nodeArgs);
 	}
 	else if (node->childNodeList.front()->value == "CAR")
 	{
-		node = elements.front()->childNodeList.front();
+		node = nodeArgs.front()->childNodeList.front();
 	}
 	else if (node->childNodeList.front()->value == "CDR")
 	{
-		elements.front()->childNodeList.pop_front();
-		node = elements.front();
+		nodeArgs.front()->childNodeList.pop_front();
+		node = nodeArgs.front();
 	}
 	else if (node->childNodeList.front()->value == "NTH")
 	{
-		node = func_NTH(elements);
+		node = func_NTH(nodeArgs);
 	}
-
+	else if (node->childNodeList.front()->value == "CONS")
+	{
+		node = func_CONS(nodeArgs);
+	}
+	else if (node->childNodeList.front()->value == "REVERSE")
+	{
+		node = func_REVERSE(nodeArgs);
+	}
+	else if (node->childNodeList.front()->value == "APPEND")
+	{
+		node = func_APPEND(nodeArgs);
+	}
+	else if (node->childNodeList.front()->value == "LENGTH")
+	{
+		node = func_LEGNTH(nodeArgs);
+	}
+	else if (node->childNodeList.front()->value == "MEMBER")
+	{
+		node = func_MEMBER(nodeArgs);
+	}
+	else if (node->childNodeList.front()->value == "ASSOC")
+	{
+		node = func_ASSOC(nodeArgs);
+	}
+	else if (node->childNodeList.front()->value == "REMOVE")
+	{
+		node = func_REMOVE(nodeArgs);
+	}
+	else if (node->childNodeList.front()->value == "SUBST")
+	{
+		node = func_SUBST(nodeArgs);
+	}
 	return node;
 
 }
